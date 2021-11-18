f5620b3280095c3ed76b534468f3ee251701f162
Juergen Hench <juergen.hench@usb.ch>
2021-11-18 15:30:52 +0100

    update

--- gita_nanodip_dependencies.sh
+++ gita_nanodip_dependencies.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #
 echo "This will push everything in "`pwd`" to GitHub..."
-git remote set-url origin https://git@github.com/neuropathbasel/nanodip_dependencies
+#git remote set-url origin git@github.com:neuropathbasel/X86_64_MinKNOW_GUI_4.3.26_install_script
+git remote set-url origin git@github.com:neuropathbasel/nanodip_dependencies
 git add -A
 git commit -m "update"
 git push
