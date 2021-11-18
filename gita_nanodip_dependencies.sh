cf18105392d456d6d9d4aa5fa192757e877273c8
Juergen Hench <juergen.hench@usb.ch>
2021-11-18 15:28:32 +0100

    update

--- gita_nanodip_dependencies.sh
+++ gita_nanodip_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 echo "This will push everything in "`pwd`" to GitHub..."
-git remote set-url origin git@https://github.com/neuropathbasel/nanodip_dependencies
+git remote set-url origin https://git@github.com/neuropathbasel/nanodip_dependencies
 git add -A
 git commit -m "update"
 git push
